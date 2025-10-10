b4387016d17efc22141bf5cbfb6e39f9bebcc908
nima <ssu87556@gmail.com>
2025-10-11 03:29:21 +0330

    Add feature profile dialog edit and save profile image

--- ui/navigation_bar.py
+++ ui/navigation_bar.py
@@ -1,11 +1,17 @@
-from PySide6.QtGui import QAction, QIcon, QFont
+from PySide6.QtGui import QAction, QIcon, QFont, QPixmap
 from PySide6.QtCore import QSize, Signal, Qt
 from PySide6.QtWidgets import (
     QToolBar, QLineEdit, QSizePolicy, QMenu, QInputDialog, QWidgetAction,
-    QPushButton, QWidget, QHBoxLayout, QTabBar
+    QPushButton, QWidget, QHBoxLayout, QTabBar, QLabel, QVBoxLayout, QDialog, QFileDialog
 )
 from .settings import Settings
 from .themes import light_theme, dark_theme
+
+import os
+from .profile_dialog import CustomizeProfile
+
+        
+
 
 class NavigationBar(QToolBar):
     url_submitted = Signal(str)
@@ -145,17 +151,36 @@ class NavigationBar(QToolBar):
             self.profile_menu.exec(action_widget.mapToGlobal(action_widget.rect().bottomLeft()))
 
     def _populate_profile_menu(self, selected_profile=None):
+        print(selected_profile)
         self.profile_menu.clear()
+        self.profile = selected_profile
+        path = f"profile_data/{self.profile}/profile/image.png"
+        print(path)
+        if os.path.exists(path):
+            self.profile_btn.setIcon(QIcon(path))
+        else:
+            self.profile_btn.setIcon(QIcon("assets/icons/profile.png"))
+        
         for profile in self.profiles:
             display_name = f"*{profile}" if profile == selected_profile else profile
             act = QAction(display_name, self)
             act.triggered.connect(lambda checked, p=profile: self.profile_selected.emit(p))
             self.profile_menu.addAction(act)
         self.profile_menu.addSeparator()
+        customize_profile = QAction("✏️ Customize Profile", self)
+        customize_profile.triggered.connect(self._customize_profile)
+        self.profile_menu.addAction(customize_profile)
+        
+        self.profile_menu.addSeparator()
         add_profile = QAction("➕ Add New Profile", self)
         add_profile.triggered.connect(self._add_new_profile)
         self.profile_menu.addAction(add_profile)
+        
+    def _customize_profile(self):
+        if self.profile is not None:
+            CustomizeProfile(self.profile, self).show()
 
+    
     def _add_new_profile(self):
         name, ok = QInputDialog.getText(self, "New Profile", "Enter profile name:")
         if ok and name.strip():
